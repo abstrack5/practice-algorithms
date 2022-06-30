6b963cb454d3270d2b62d1a36d1b26068ee0adff
Abstrack <Armon_ah05@outlook.com>
2022-06-29 23:58:52 -0400

    added bubble sort

---
+++ Sort/Bubble/bubble.js
@@ -0,0 +1,21 @@
+
+const numbers = [
+  54, 8, 87, 88, 100, 98, 17, 72, 67, 42, 40, 34, 55, 18, 33, 94, 59, 68, 92, 2,
+];
+
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+console.log(bubbleSort(numbers));
+// bubbleSort(numbers)
